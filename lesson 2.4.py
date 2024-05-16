ee73bc6cff1f99e947a03c7e52ac47a3dd8cc581
Alexey-nsk <168356430+Alexey-nsk@users.noreply.github.com>
2024-05-16 20:22:45 +0700

    Add files via upload

---
+++ lesson 2.4.py
@@ -0,0 +1,7 @@
+car = ['BMW', 'MB', 'LADA', 'KIA', 'Honda']
+for i in range(len(car)):
+    print('Я езжу на автомобиле марки ' + car[i])
+car_count = 0
+for j in range(1, 11, 2):
+    car_count += 10
+    print(car_count)
