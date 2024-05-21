49be8b0a08215d529c8039000b6d8f42fb75aaf6
Alexey-nsk <168356430+Alexey-nsk@users.noreply.github.com>
2024-05-21 16:52:21 +0700

    Update lesson 2.4.py

--- lesson 2.4.py
+++ lesson 2.4.py
@@ -1,6 +1,6 @@
 car = ['BMW', 'MB', 'LADA', 'KIA', 'Honda']
-for i in range(len(car)):
-    print('Я езжу на автомобиле марки ' + car[i])
+for i in (car):
+    print('Я езжу на автомобиле марки ' + (i)
 car_count = 0
 for j in range(1, 11, 2):
     car_count += 10
